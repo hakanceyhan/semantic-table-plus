6e701d76d68da63e18920ab54b3d2e2e5b20eb5b
hakanceyhan <49584706+hakanceyhan@users.noreply.github.com>
2021-03-15 21:51:50 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
-import SemanticDndTable from "./SemanticDndTable"
+import SemanticTablePlus from "./SemanticTablePlus"
 import 'semantic-ui-css/semantic.min.css'
 import {data, columns, } from "./data"
-
-
-
+import { Button } from "semantic-ui-react"
 
 class App extends Component {
+
+	constructor(props) {
+        super(props);
+        this.state = {};
+
+        this.rowSelector = this.rowSelector.bind(this); 
+    }
+
+	rowSelector = (data) => {console.log(data)}
+
 	render() {
 		return (
 			<div>
-				<SemanticDndTable
+				<SemanticTablePlus
 					data={data}
 					columns={columns}
 					pageSize={15}
+					exportable={true}
 					searchable={true}
+					TableProps={{celled:true}}
+					PaginationProps={{}}
+					SeachInputProps={{}}
+					ExportButtonProps={{}}
+					onRowSelect={this.rowSelector}
+					cellRenderer={{sepal_length:(c,i)=>{return <Button key={i}>{c}</Button>}}}	
 				/>
 			</div>
 		);
